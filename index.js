e87f8ec2c470a16c10d7d1eba8d13173380809ce
Your Name <benyebai@gmail.com>
2022-05-30 22:45:18 -0700

    first commiit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+const db = require('./queries')
+const port = 3001
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+    res.header("Access-Control-Allow-Headers", "url, Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin");
+    next();
+});
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers)
+
+app.post('/checkemail', db.checkEmailAvailable)
+
+app.post('/adduser', db.addUser)
+
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
---
+++ queries.js
@@ -0,0 +1,56 @@
+const { response } = require('express')
+
+require('dotenv').config()
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+})
+
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM account_info ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(results.rows)
+  })
+}
+
+const checkEmailAvailable = (request, response) =>  {
+  const email = request.body.email
+  const username = request.body.username
+
+  pool.query('SELECT * FROM account_info WHERE email = $1 OR username = $2', [email, username], (error, results) => {
+    if (error) {
+      throw error
+    }
+
+    response.status(200).send(results)
+  })
+
+}
+
+const addUser = (request, response) =>  {
+  const email = request.body.email
+  const password = request.body.password
+  const username = request.body.username
+
+  pool.query('INSERT INTO account_info (email, password, username) VALUES ($1, $2, $3)', [email, password, username], (error, results) => {
+    if (error) {
+      throw error
+    }
+
+    response.status(200).send("user added")
+  })
+
+}
+
+module.exports = {
+  getUsers,
+  checkEmailAvailable,
+  addUser
+  
+}
